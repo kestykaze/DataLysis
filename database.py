00d188afe1c7b8802863d30a5c3f3a9e0d88b3f2
kestykaze <kestykaze377@gmail.com>
2026-04-29 22:01:02 +0100

    fix host variable

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import os
 
 def get_connection():
     connection = pymysql.connect(
-        host=os.environ.get('MYSQLHOSTE', 'localhost'),
+        host=os.environ.get('MYSQLHOSTE') or os.environ.get('MYSQLHOST', 'localhost'),
         user=os.environ.get('MYSQLUSER', 'amazkest'),
         password=os.environ.get('MYSQLPASSWORD', '2Am2y4l2dcj2@!'),
-        database=os.environ.get('MYSQLDATABASE', 'collecte_db'),
+        database=os.environ.get('MYSQLDATABASE', 'railway'),
         port=int(os.environ.get('MYSQLPORT', 3306)),
         cursorclass=pymysql.cursors.DictCursor
     )
