ca1688e78e63d6483cad3fcd2a679427e150119c
kestykaze <kestykaze377@gmail.com>
2026-04-29 21:33:10 +0100

    fix MYSQLHOSTE

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 
 def get_connection():
     connection = pymysql.connect(
-        host=os.environ.get('MYSQLHOST', 'localhost'),
+        host=os.environ.get('MYSQLHOSTE', 'localhost'),
         user=os.environ.get('MYSQLUSER', 'amazkest'),
         password=os.environ.get('MYSQLPASSWORD', '2Am2y4l2dcj2@!'),
         database=os.environ.get('MYSQLDATABASE', 'collecte_db'),
